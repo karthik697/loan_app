83e1398ae0d6481297bfcaf3da4eee79e9e36e0a
Dhana1924 <107107719+Dhana1924@users.noreply.github.com>
2025-01-01 22:40:01 +0530

    creating a loan app
    creating a loan app

---
+++ predictions.py
@@ -0,0 +1,49 @@
+from flask import Flask, request
+import pickle
+#import sklearn
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World V2 !</p>"
+
+
+@app.route("/ping")
+def pinger():
+    return {"MESSAGE": "Hi I am pinging V2...."}
+
+model_pickle= open("artefacts/classifier.pkl", 'rb')
+clf = pickle.load(model_pickle)
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    loan_req = request.get_json()
+
+    if loan_req['Gender'] == "Male":
+        gender = 0
+    else:
+        gender = 1
+
+    if loan_req['Married'] == "No":
+        married = 0
+    else:
+        married = 1
+
+    applicant_income = loan_req['ApplicantIncome']
+    credit_history = loan_req['Credit_History']
+    loan_amount = loan_req['LoanAmount']
+
+    input_data = [[gender, married, applicant_income,loan_amount , credit_history]]
+    prediction = clf.predict(input_data)
+
+    if prediction == 0:
+        pred = "Rejected"
+    else:
+        pred = "Accepted"
+
+    return {"loan_approval_status": pred}
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
---
+++ test_square.py
@@ -0,0 +1,10 @@
+import pytest
+from square import get_square
+
+def test_get_square():
+    a =4
+    assert get_square(a) == 16
+
+
+
+
